92ee247584a7886e0800e4a82d016ae9bb43790e
NSLynn <mango@MacBook-Pro.local>
2015-05-04 19:36:57 +0800

    Initial Commit

---
+++ SideMenuFromGitHub/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SideMenuFromGitHub
+//
+//  Created by Nirvana on 5/4/15.
+//  Copyright (c) 2015 Nirvana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
